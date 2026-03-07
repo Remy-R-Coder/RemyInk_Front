4e9a4cd99abbf3af9968f5c9a8a3189d578cbe10
kimxons <kitongameshack9@gmail.com>
2026-03-07 14:06:57 +0300

    fix

--- components/slide/Slide.jsx
+++ components/slide/Slide.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Slide.scss";
-import Slider from "infinite-react-carousel";
 
-const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+const Slide = ({ children, slidesToShow = 1 }) => {
+  const normalizedSlidesToShow = Number(slidesToShow) > 0 ? Number(slidesToShow) : 1;
+  const itemMinWidth = `calc((100% - ${(normalizedSlidesToShow - 1) * 16}px) / ${normalizedSlidesToShow})`;
+
   return (
     <div className="slide">
       <div className="container">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
-          {children}
-        </Slider>
+        <div className="slide__track">
+          {React.Children.map(children, (child, index) => (
+            <div className="slide__item" style={{ minWidth: itemMinWidth }} key={index}>
+              {child}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
