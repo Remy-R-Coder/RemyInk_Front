a6b9827c805c1f5e18fc8a2b81021981bdee6d8d
Remy-R-Coder <remymoses6@gmail.com>
2026-04-09 16:00:44 +0300

    changes in navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -364,17 +364,20 @@ const Navbar = () => {
                           </svg>
                           Profile
                         </Link>
+                        
+                        {user.role !== "CLIENT" && (
+                          <Link href="/orders" className="navbar__dropdown-item">
+                            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+                              <polyline points="14 2 14 8 20 8" />
+                              <line x1="16" y1="13" x2="8" y2="13" />
+                              <line x1="16" y1="17" x2="8" y2="17" />
+                            </svg>
+                            Orders
+                          </Link>
+                        )}
 
-                        <Link href="/orders" className="navbar__dropdown-item">
-                          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-                            <polyline points="14 2 14 8 20 8" />
-                            <line x1="16" y1="13" x2="8" y2="13" />
-                            <line x1="16" y1="17" x2="8" y2="17" />
-                          </svg>
-                          Orders
-                        </Link>
-
+                        
                         {user.is_freelancer && (
                           <Link href="/earnings" className="navbar__dropdown-item">
                             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
